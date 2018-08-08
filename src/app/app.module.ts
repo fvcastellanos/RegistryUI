35add03286114d4770f2d1d61a02e37b814b0603
Francisco Castellanos <fvcastellanos@gmail.com>
2018-08-07 19:34:49 -0600

    fix menu navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { IndexComponent } from './index/index.component';
 import { RouterModule } from '../../node_modules/@angular/router';
 import { ROUTES } from './app.routes';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogComponent,
-    IndexComponent
+    IndexComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
